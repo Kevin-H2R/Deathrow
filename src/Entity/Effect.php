e0d3a387cb34e51acbb8838f19ca9568718a81d5
Kevin Herrbach <herrbach.kevin@gmail.com>
2019-11-04 15:01:24 +0100

    Adds effect entity

---
+++ src/Entity/Effect.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EffectRepository")
+ */
+class Effect
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $min;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $max;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Equipment", inversedBy="effects")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $equipment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMin(): ?int
+    {
+        return $this->min;
+    }
+
+    public function setMin(int $min): self
+    {
+        $this->min = $min;
+
+        return $this;
+    }
+
+    public function getMax(): ?int
+    {
+        return $this->max;
+    }
+
+    public function setMax(int $max): self
+    {
+        $this->max = $max;
+
+        return $this;
+    }
+
+    public function getEquipment(): ?Equipment
+    {
+        return $this->equipment;
+    }
+
+    public function setEquipment(?Equipment $equipment): self
+    {
+        $this->equipment = $equipment;
+
+        return $this;
+    }
+}
