06e75c200a855c063e2f2e52dfefbaa5d13344d5
Kevin Herrbach <herrbach.kevin@gmail.com>
2019-11-06 00:11:13 +0100

    Adds type column in Equipment

--- src/Entity/Equipment.php
+++ src/Entity/Equipment.php
@@ -48,6 +48,11 @@ class Equipment
      */
     private $image_id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
     public function __construct()
     {
         $this->recipes = new ArrayCollection();
@@ -160,6 +165,18 @@ class Equipment
     public function setImageId(?int $image_id): self
     {
         $this->image_id = $image_id;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
 
         return $this;
     }
---
+++ src/Migrations/Version20191105230648.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191105230648 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE equipment ADD type VARCHAR(255) NOT NULL, CHANGE cloth_id cloth_id INT DEFAULT NULL, CHANGE image_id image_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE item CHANGE image_id image_id INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE equipment DROP type, CHANGE cloth_id cloth_id INT DEFAULT NULL, CHANGE image_id image_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE item CHANGE image_id image_id INT DEFAULT NULL');
+    }
+}
